6ec6e980f5c8b924c026f9f04512484376f8ebc7
Gustavo Fernandes <gustavofernandes.f@outlook.com>
2022-05-05 18:11:12 -0300

    funções criadas

--- exercicios/exer03.py
+++ exercicios/exer03.py
@@ -1,31 +1,8 @@
-lista_chamada = ['Gustavo', 'Ricardo', 'Felipe', 'Emanuel'
-                 'Laryssa', 'Gaby', 'Ruan', 'Ramom', 'João Miguel'
+lista_chamada = ['Gustavo', 'Ricardo', 'Felipe', 'Emanuel',
+                 'Laryssa', 'Gaby', 'Ruan', 'Ramom', 'João Miguel',
                  'Rafael']
-presenca = []
-lista_presenca = []
-lista_falta = []
-i = 0
 
-while i < len(lista_chamada):
-    presenca_aluno = input(lista_chamada[i] + " Esta presente? [S/N]") 
-    if presenca_aluno == 'S' or presenca_aluno == 's':
-        presenca.append(True)
-    elif presenca_aluno == 'N' or presenca_aluno == 'n':
-        presenca.append(False)
-    i = i +1
-
-j = 0
-while j < len(presenca):
-    if presenca[j] == True:
-        lista_presenca.append(lista_chamada[j])
-    elif presenca[j] == False:
-        lista_falta.append(lista_chamada[j])
-    
-    j = j + 1 
-
-# MOSTRA LISTA DE PRESENÇA E LISTA DE FALTAS
-# FAZER FUNÇÕES PARA CHAMADA E PARA AS LISTAS
-def presencas():
+def presencas(lista_chamada):
     i = 0
     while i < len(lista_chamada):
         presenca = []
@@ -37,13 +14,26 @@ def presencas():
         i = i + 1 
     return presenca
 
-def listas(presensa):
-    lista_presenca = []
-    lista_falta = []
-    i = 0 
-    while i < len(presenca):
-        if presenca[i] == True:
-            lista_presenca.append(lista_chamada[i])
-        elif presenca[i] == False:
-            lista_falta.append(lista_chamada[i])
-        i = i + 1 
+def mostra_listas(presenca):
+    lista_faltas = []
+    lista_presente = []
+    i = 0
+    print("VER LISTA:")
+    print("[1] Lista de Alunos presentes")
+    print("[2] Listas de Alunos que faltaram")
+    opcao = int(input("Informe a opção: "))
+    if opcao == 1:
+        while i < len(presenca):
+            if presenca == True:
+                lista_presente.append(lista_chamada[i])
+            i = i + 1
+        return lista_presente
+    elif opcao == 2:
+        while i < len(presenca):
+            if presenca == False:
+                lista_faltas.append(lista_chamada[i])
+            i = i + 1
+        return lista_faltas
+
+presencas = presencas(lista_chamada)
+print(mostra_listas(presencas))
